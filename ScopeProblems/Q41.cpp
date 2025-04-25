93036456f098a135caa1551a753c6b485431587e
vijay-pankaj <vijaypankaj960@gmail.com>
2025-04-26 01:35:32 +0530

    modified

--- ScopeProblems/Q41.cpp
+++ ScopeProblems/Q41.cpp
@@ -3,17 +3,16 @@ using namespace std;
 int main() { 
     for (int i = 1; i <= 5; i++) { 
         for (int j = 1; j <= i; j++) 
-            cout << "*";
+            cout << "* ";
         cout << endl;
     }
     return 0;
 }
 // o/p 
 /*
-*
-**
-***
-****
-*****
-
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
 */
