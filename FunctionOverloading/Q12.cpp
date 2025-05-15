071ecbf15989de041170fcd852f22a646e3a016f
vijay <vijaypankaj960@gmail.com>
2025-05-15 15:20:23 +0530

    changes in fxn_OL

--- FunctionOverloading/Q12.cpp
+++ FunctionOverloading/Q12.cpp
@@ -1,15 +1,17 @@
 // 12. Write overloaded functions to print values of different types (int, float, char, and string) in a formatted way.
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void differentDtype(int n1,float n2 ,char ch, string str){
-cout<<"integer "<<n1<<" float "<<n2<<" char "<<ch<<" string "<<str;
+void differentDtype(int n1, float n2, char ch, string str)
+{
+    cout << "integer " << n1 << " float " << n2 << " char " << ch << " string " << str;
 }
-int main(){
-int n1;
-float n2;
-char ch;
-string str;
-cout<<"enter variable valuew in a formate: ";
-cin>>n1>>n2>>ch>>str;
-differentDtype(n1,n2,ch,str);
+int main()
+{
+    int n1;
+    float n2;
+    char ch;
+    string str;
+    cout << "enter variable valuew in a formate: ";
+    cin >> n1 >> n2 >> ch >> str;
+    differentDtype(n1, n2, ch, str);
 }
--- FunctionOverloading/Q6.cpp
+++ FunctionOverloading/Q6.cpp
@@ -1,21 +1,25 @@
 // 6. Implement overloaded functions to concatenate two strings, two characters, and a string with a character
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-string concatenate(string a, string b) {
+string concatenate(string a, string b)
+{
     return a + b;
 }
 
-string concatenate(char a, char b) {
+string concatenate(char a, char b)
+{
     return string(1, a) + b;
 }
 
-string concatenate(string a, char b) {
+string concatenate(string a, char b)
+{
     return a + b;
 }
 
-int main() {
-    cout << "String + String: " << concatenate("Hello, ", "World!") << endl;
-    cout << "Char + Char: " << concatenate('A', 'B') << endl;
-    cout << "String + Char: " << concatenate("Test", '!') << endl;
+int main()
+{
+    cout << concatenate("Hello, ", "World!") << endl;
+    cout << concatenate('A', 'B') << endl;
+    cout << concatenate("Test", '!') << endl;
     return 0;
 }
